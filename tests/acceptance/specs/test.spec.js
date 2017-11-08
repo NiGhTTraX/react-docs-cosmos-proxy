1c272170dad00419fbaea66d3752ca6ecf849e4c
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-11-08 15:29:18 +0200

    Make acceptance tests pass

--- tests/acceptance/specs/test.spec.js
+++ tests/acceptance/specs/test.spec.js
@@ -2,6 +2,6 @@ import { mugshotSuite, loadFixture } from '../helpers.js';
 
 mugshotSuite('Test', it => {
   it('should work', async () => {
-    await loadFixture('one-prop', 'base');
+    await loadFixture('acceptance/one-prop', 'base');
   });
 });
