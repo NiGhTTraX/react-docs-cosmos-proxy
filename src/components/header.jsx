6f4d24ebf0b683296092ed7dd1157fa006c8826f
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-11-08 14:25:30 +0200

    Show the header content when pressing the toggle

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import './header.less';
 
@@ -9,18 +10,27 @@ export default class Header extends Component {
     children: PropTypes.node.isRequired
   };
 
+  state = {
+    hidden: true
+  };
+
   render() {
     const { children } = this.props;
+    const { hidden } = this.state;
 
     return <div className="header">
-      <div className="content">
+      <div className={classNames('content', { hidden })}>
         {children}
       </div>
-      <div className="toggle">
+      <button className="toggle" onClick={this.onToggle}>
         <svg viewBox="0 0 1792 1792">
           <path d="M1395 864q0 13-10 23l-466 466q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l393 393 393-393q10-10 23-10t23 10l50 50q10 10 10 23zm0-384q0 13-10 23l-466 466q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l393 393 393-393q10-10 23-10t23 10l50 50q10 10 10 23z" fill="currentColor" />
         </svg>
-      </div>
+      </button>
     </div>;
+  }
+
+  onToggle = () => {
+    this.setState({ hidden: !this.state.hidden });
   }
 }
--- tests/unit/components/header.spec.js
+++ tests/unit/components/header.spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Simulate } from 'react-dom/test-utils';
 import Header from '../../../src/components/header.jsx';
 import { $render } from '../helpers/rendering.js';
 
@@ -12,6 +13,16 @@ describe('Header', () => {
 
   it('should render its children', () => {
     expect($header.text()).to.equal('foobar');
+  });
+
+  it('should initially hide the content', () => {
+    expect($header.find('.content').hasClass('hidden')).to.be.true;
+  });
+
+  it('should show the content when toggling', () => {
+    Simulate.click($header.find('.toggle')[0]);
+
+    expect($header.find('.content').hasClass('hidden')).to.be.false;
   });
 });
 
