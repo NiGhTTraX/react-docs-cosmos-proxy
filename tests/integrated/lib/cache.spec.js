40f2aaad45e438c87c0c4b31dea489caa3e46623
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-11-09 18:29:58 +0200

    Nest current cache tests under a no namespace block

--- tests/integrated/lib/cache.spec.js
+++ tests/integrated/lib/cache.spec.js
@@ -7,35 +7,37 @@ const Implementations = [
 Implementations.forEach(([name, Implementation]) => describe(name, () => {
   let instance;
 
-  beforeEach(() => {
-    instance = new Implementation();
-  });
+  describe('no namespace', () => {
+    beforeEach(() => {
+      instance = new Implementation();
+    });
 
-  afterEach(() => {
-    instance.clear();
-  });
+    afterEach(() => {
+      instance.clear();
+    });
 
-  it('should not return an undefined key', () => {
-    expect(instance.get('undefined')).to.be.undefined;
-  });
+    it('should not return an undefined key', () => {
+      expect(instance.get('undefined')).to.be.undefined;
+    });
 
-  it('should set a simple value', () => {
-    instance.set('simple', 1);
+    it('should set a simple value', () => {
+      instance.set('simple', 1);
 
-    expect(instance.get('simple')).to.equal(1);
-  });
+      expect(instance.get('simple')).to.equal(1);
+    });
 
-  it('should set a complex value', () => {
-    instance.set('complex', { foo: 'bar' });
+    it('should set a complex value', () => {
+      instance.set('complex', { foo: 'bar' });
 
-    expect(instance.get('complex')).to.deep.equal({ foo: 'bar' });
-  });
+      expect(instance.get('complex')).to.deep.equal({ foo: 'bar' });
+    });
 
-  it('should clear everything', () => {
-    instance.set('you-see-me', 'now');
+    it('should clear everything', () => {
+      instance.set('you-see-me', 'now');
 
-    instance.clear();
+      instance.clear();
 
-    expect(instance.get('you-see-me')).to.be.undefined;
+      expect(instance.get('you-see-me')).to.be.undefined;
+    });
   });
 }));
