f7d9d0394fb81079cb4dd428cfe488a0759010b3
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-12-02 18:54:42 +0100

    Add acceptance fixture for component with no props

---
+++ playground/components/acceptance/nothing.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Nothing extends Component {
+  render() {
+    return <div>
+      {"I don't have any docs"}
+    </div>;
+  }
+}
